d8a802777b1a5c8a5207f205721ca2c63c724527
aziza-22 <halazabnoch123@gmail.com>
2025-12-25 02:40:57 +0400

    Remove pr1 folder

--- client/vitest.config.js
+++ client/vitest.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vitest/config";
 
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  test: {
-    globals: true,
-    environment: "jsdom",
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
   },
 });
